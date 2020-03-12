ceca134beda872d0fab005c65ab7e4f8a805ea61
logchan <logchan@outlook.com>
2020-03-12 13:09:10 -0400

    update

--- Arduino/src/Arduchuni/Arduchuni.ino
+++ Arduino/src/Arduchuni/Arduchuni.ino
@@ -1,26 +1,29 @@
 #include "SevenSegmentLed.hpp"
 
-#define INDICATOR_START 8
-#define SENSOR_START 0
-#define N_SENSORS 1
-#define LED_START 22
+#define INDICATOR_START 22
+#define SENSOR_START A0
+#define N_SENSORS 2
+#define LED_START 28
 #define INDICATOR_BRIGHTNESS 128
+#define LED_EVERY 100
 
 const int ledPins[4+7] = {
-  LED_START, LED_START+1, LED_START+2, LED_START+3,
-  LED_START+4, LED_START+5, LED_START+6, LED_START+7, LED_START+8, LED_START+9, LED_START+10
+  LED_START, LED_START+1, LED_START+2, LED_START+3, LED_START+4, LED_START+5, LED_START+6,
+  LED_START+7, LED_START+8, LED_START+9, LED_START+10
 };
-const SevenSegmentLed<4> led(ledPins, ledPins+4);
+SevenSegmentLed<4> led(ledPins+7, ledPins);
 
 void setup() {
   for (int i = 0; i < N_SENSORS; ++i) {
     pinMode(i + INDICATOR_START, OUTPUT);
   }
-  for (int i = 0; i < sizeof(ledPins)/sizeof(*ledPins), ++i) {
+  for (int i = 0; i < sizeof(ledPins)/sizeof(*ledPins); ++i) {
     pinMode(ledPins[i], OUTPUT);
   }
   Serial.begin(9600);
 }
+
+int led_counter = 0;
 
 void loop() {
   for (int i = 0; i < N_SENSORS; ++i) {
@@ -31,10 +34,13 @@ void loop() {
     int brightness = val > 1000 ? INDICATOR_BRIGHTNESS : 0;
     analogWrite(pInd, brightness);
 
-    if (i == 0) {
+    if (led_counter % LED_EVERY == 0) {
+      led_counter = 0;
       led.setNumber(val);
     }
+    led_counter += 1;
   }
 
-  delay(10);
+  led.loop();
+  delay(5);
 }
--- Arduino/src/Arduchuni/SevenSegmentLed.hpp
+++ Arduino/src/Arduchuni/SevenSegmentLed.hpp
@@ -17,21 +17,17 @@ template<int N>
 class SevenSegmentLed {
     public:
     int numberData[10] = {
-        0b0111111, 0b1011011, 0b1011011, 0b1001111, 0b1100110, 
+        0b0111111, 0b0000110, 0b1011011, 0b1001111, 0b1100110, 
         0b1101101, 0b1111101, 0b0000111, 0b1111111, 0b1101111
-    }
+    };
 
-    SevenSegmentLed(int* digitPins, int* dataPins) : m_digitPins(digitPins), m_dataPins(dataPins) {
+    SevenSegmentLed(const int* digitPins, const int* dataPins) {
+      m_digitPins = digitPins;
+      m_dataPins = dataPins;
     }
 
     void setDigit(int digit, int data) {
-        for (int i = 0; i < N; ++i) {
-            digitalWrite(m_digitPins[i], digit == i ? HIGH : LOW);
-        }
-        for (int i = 0; i < 7; ++i) {
-            digitalWrite((data & 1) > 0 ? LOW : HIGH);
-            data >>= 1;
-        }
+        m_data[digit] = data;
     }
 
     void setEmpty(int digit) {
@@ -58,7 +54,28 @@ class SevenSegmentLed {
         }
     }
 
+    void loop() {
+      updateDigit(m_loop_i);
+      m_loop_i = (m_loop_i+1) % N;
+    }
+
     private:
-    int* m_digitPins;
-    int* m_dataPins;
-}
+
+    void updateDigit(int digit) {
+      int data = m_data[digit];
+      
+      for (int i = 0; i < N; ++i) {
+            digitalWrite(m_digitPins[i], HIGH);
+        }
+        for (int i = 0; i < 7; ++i) {
+            digitalWrite(m_dataPins[i], (data & 1) == 0 ? LOW : HIGH);
+            data >>= 1;
+        }
+        digitalWrite(m_digitPins[digit], LOW);
+    }
+    
+    const int* m_digitPins;
+    const int* m_dataPins;
+    int m_data[N];
+    int m_loop_i = 0;
+};
