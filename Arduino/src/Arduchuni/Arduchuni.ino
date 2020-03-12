3e2e18ac5d91c84358e4c6699a9bbc2047ba41d0
logchan <logchan@outlook.com>
2020-03-12 13:23:24 -0400

    update

--- Arduino/src/Arduchuni/Arduchuni.ino
+++ Arduino/src/Arduchuni/Arduchuni.ino
@@ -1,46 +1,62 @@
+#define DEV_VERSION
+
+#define LOOP_DELAY 5
+#define N_SENSORS 6
+const int sensorPins[N_SENSORS] = { A0, A1, A2, A3, A4, A5 };
+
+#ifdef DEV_VERSION
+
+#define INDICATOR_BRIGHTNESS 128
+#define INDICATOR_THRESHOLD 1000
+const int indicatorPins[N_SENSORS] = { 22, 23, 24, 25, 26, 27 };
+
 #include "SevenSegmentLed.hpp"
 
-#define INDICATOR_START 22
-#define SENSOR_START A0
-#define N_SENSORS 2
 #define LED_START 28
-#define INDICATOR_BRIGHTNESS 128
 #define LED_EVERY 100
-
+int led_counter = 0;
 const int ledPins[4+7] = {
   LED_START, LED_START+1, LED_START+2, LED_START+3, LED_START+4, LED_START+5, LED_START+6,
   LED_START+7, LED_START+8, LED_START+9, LED_START+10
 };
 SevenSegmentLed<4> led(ledPins+7, ledPins);
+#endif
 
 void setup() {
+  Serial.begin(9600);
+
+#ifdef DEV_VERSION
   for (int i = 0; i < N_SENSORS; ++i) {
-    pinMode(i + INDICATOR_START, OUTPUT);
+    pinMode(indicatorPins[i], OUTPUT);
   }
   for (int i = 0; i < sizeof(ledPins)/sizeof(*ledPins); ++i) {
     pinMode(ledPins[i], OUTPUT);
   }
-  Serial.begin(9600);
+#endif
 }
 
-int led_counter = 0;
-
 void loop() {
+  int values[N_SENSORS];
+
   for (int i = 0; i < N_SENSORS; ++i) {
-    int pSensor = i + SENSOR_START;
-    int pInd = i + INDICATOR_START;
-
-    int val = analogRead(pSensor);
-    int brightness = val > 1000 ? INDICATOR_BRIGHTNESS : 0;
-    analogWrite(pInd, brightness);
-
-    if (led_counter % LED_EVERY == 0) {
-      led_counter = 0;
-      led.setNumber(val);
-    }
-    led_counter += 1;
+    values[i] = analogRead(sensorPins[i]);
   }
 
+  Serial.write((void*)values, sizeof(values));
+  Serial.flush();
+
+#ifdef DEV_VERSION
+  for (int i = 0; i < N_SENSORS; ++i) {
+    analogWrite(indicatorPins[i], values[i] > INDICATOR_THRESHOLD ? INDICATOR_BRIGHTNESS : 0);
+  }
+
+  if (led_counter % LED_EVERY == 0) {
+    led_counter = 0;
+    led.setNumber(values[0]);
+  }
+  led_counter += 1;
   led.loop();
-  delay(5);
+#endif
+
+  delay(LOOP_DELAY);
 }
